bac5dc06671532bdf3cfd4b6c8c2ab9469bc43ea
kia0919 <john0256@naver.com>
2024-03-18 17:16:21 +0900

    2024.03.18

--- gradle/app/src/main/java/springworkspace/App.java
+++ gradle/app/src/main/java/springworkspace/App.java
@@ -3,11 +3,22 @@
  */
 package springworkspace;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+// @NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor //final을 지정된 생성자
+@Builder
 class Person {
-    
+    private final String name;
+    private int age;
 }
 
 public class App {
@@ -17,5 +28,10 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        Person person = new Person("홍길동", 20);
+        person = new Person("고길동");
+
+        person = Person.builder().name("홍길동").age(20).build();
     }
 }
